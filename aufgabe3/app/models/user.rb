66c0279dbae341261c2dafe987a2cc93aa27be68
Alex Mantel <alexmantel93@gmail.com>
2016-05-08 21:54:50 +0200

    Add default value to 'is_admin' in user

--- aufgabe3/app/models/user.rb
+++ aufgabe3/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  after_initialize :set_default_is_admin
 	before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -6,4 +7,8 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def set_default_is_admin
+    self.is_admin ||= false
+  end
 end
